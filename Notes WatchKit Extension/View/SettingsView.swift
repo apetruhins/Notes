77f11f7c08cceb171806aee14100c79988dc8144
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-07-07 22:30:09 +0300

    Improved settings

--- Notes WatchKit Extension/View/SettingsView.swift
+++ Notes WatchKit Extension/View/SettingsView.swift
@@ -12,15 +12,7 @@ struct SettingsView: View {
     // MARK: - Properties
     
     @AppStorage("lineCount") var lineCount: Int = 1
-    
-    @State private var value: Float = 1.0
 
-    // MARK: - Functions
-    
-    func update() {
-        lineCount = Int(value)
-    }
-    
     // MARK: - Body
     
     var body: some View {
@@ -35,11 +27,10 @@ struct SettingsView: View {
             
             // Slider
             Slider(value: Binding(get: {
-                self.value
+                Double(self.lineCount + 1)
             }, set: { newValue in
-                self.value = newValue
-                self.update()
-            }), in: 1...4, step: 1)
+                self.lineCount = newValue < 2 ? 1 : Int(newValue) - 1
+            }), in: 1...5, step: 1)
                 .accentColor(.accentColor)
             
         } //: VStack
